258482b8daf6f12d2d9da084e3704fb5001d3157
SarkaKv <145465445+SarkaKv@users.noreply.github.com>
2024-07-23 17:05:05 +0200

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -12,28 +12,58 @@
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+// #old version of my strjoin
+// char	*ft_strjoin(char const *s1, char const *s2)
+// {
+// 	char	*news;
+// 	size_t	start1;
+// 	size_t	start2;
+
+// 	start1 = 0;
+// 	start2 = 0;
+// 	news = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+// 	if (!news)
+// 		return (NULL);
+// 	while (start1 < ft_strlen(s1))
+// 	{
+// 		news[start1] = s1[start1];
+// 		start1++;
+// 	}
+// 	while (start1 < ((ft_strlen(s1) + ft_strlen(s2))))
+// 	{
+// 		news[start1] = s2[start2];
+// 		start1++;
+// 		start2++;
+// 	}
+// 	news[start1] = '\0';
+// 	return (news);
+// }
+
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*news;
 	size_t	start1;
 	size_t	start2;
+	size_t	increase1;
+	size_t	increase2;
 
-	start1 = 0;
-	start2 = 0;
-	news = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	increase1 = 0;
+	increase2 = 0;
+	start1 = ft_strlen(s1);
+	start2 = ft_strlen(s2);
+	news = (char *)malloc((start1 + start2 + 1) * sizeof(char));
 	if (!news)
 		return (NULL);
-	while (start1 < ft_strlen(s1))
+	while (increase1 < start1)
 	{
-		news[start1] = s1[start1];
-		start1++;
+		news[increase1] = s1[increase1];
+		increase1++;
 	}
-	while (start1 < ((ft_strlen(s1) + ft_strlen(s2))))
+	while (increase2 < start2)
 	{
-		news[start1] = s2[start2];
-		start1++;
-		start2++;
+		news[increase1+increase2] = s2[increase2];
+		increase2++;
 	}
-	news[start1] = '\0';
+	news[increase1+increase2] = '\0';
 	return (news);
 }
